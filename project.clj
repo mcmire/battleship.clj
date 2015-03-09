11747dbcccf08a5a558ffb11a37705c0857912d4
Elliot Winkler <elliot.winkler@gmail.com>
2015-03-09 10:12:32 -0600

    New project generated with `lein new`

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject battleship "0.1.0-SNAPSHOT"
+  :description "Battleship, in Clojure"
+  :url "http://github.com/mcmire/battleship"
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main battleship.core)
